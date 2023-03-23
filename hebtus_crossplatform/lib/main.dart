bbcd718e820878517a1e5d37d399b1188b0eebc9
AyaUsamaKhalifa <74725538+AyaUsamaKhalifa@users.noreply.github.com>
2023-03-23 10:50:46 +0200

    updtaed all_screen,dart

--- hebtus_crossplatform/lib/main.dart
+++ hebtus_crossplatform/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hebtus_crossplatform/screens/Creator/Tickets/tickets_assign.dart';
 import 'package:hebtus_crossplatform/theme/style.dart';
 import 'package:provider/provider.dart';
 import 'screens/all_screens.dart';
-import 'package:hebtus_crossplatform/screens/Creator/BasicInfo/basic_info.dart';
 
 void main() {
   runApp(const MyApp());
--- hebtus_crossplatform/lib/screens/all_screens.dart
+++ hebtus_crossplatform/lib/screens/all_screens.dart
@@ -1,3 +1,10 @@
 export 'Login/login_screen.dart';
 export 'SignUp/signup_screen.dart';
 export 'ForgotPassword/forgot_password.dart';
+export 'Creator/BasicInfo/basic_info.dart';
+export 'Creator/Components/creator_components.dart';
+export 'Creator/Dashboard/dashboard.dart';
+export 'Creator/Details/details.dart';
+export 'Creator/OnlineEventPage/online_event_page.dart';
+export 'Creator/Publish/publish.dart';
+export 'Creator/Tickets/tickets_assign.dart';
