3218d5ca3b1571fb5af0de426c43b3c04ccf749a
AyaUsamaKhalifa <74725538+AyaUsamaKhalifa@users.noreply.github.com>
2023-03-25 22:00:11 +0200

    landing page screen test done, another login screen test added

---
+++ hebtus_crossplatform/test/widgetTests/landing_screen_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hebtus_crossplatform/screens/LandingPage/components/categories.dart';
+import 'package:hebtus_crossplatform/screens/all_screens.dart';
+
+void main() {
+  Widget makeTestableWidget({required Widget child}) {
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets("search for the categories tabs on different screen sizes",
+      (WidgetTester tester) async {
+    await tester
+        .pumpWidget(makeTestableWidget(child: const LandingPageScreen()));
+    //find category widgets
+    Finder categoryCards = find.byType(Card);
+    //verify that all cards exists
+    expect(categoryCards, findsNWidgets(14));
+
+    //resize the window and test again
+    await tester.binding.setSurfaceSize(const Size(40, 40));
+
+    //verify no overflow in specific categories box
+
+    final healthCategoryCard = tester.renderObject(find.text("Health"));
+
+    expect((healthCategoryCard as RenderBox).size,
+        equals(healthCategoryCard.paintBounds.size));
+  });
+}
