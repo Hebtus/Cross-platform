59162224d04a495c0711b2d6a52dbbcaaf0bffb9
youssefkl12 <youssefkhaledmahmoud12@gmail.com>
2023-05-09 03:32:40 +0300

    tests

---
+++ hebtus_crossplatform/test/widgetTests/scroll_editTicket_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hebtus_crossplatform/current_user.dart';
+import 'package:hebtus_crossplatform/models/creator_events.dart';
+import 'package:hebtus_crossplatform/screens/Creator/BasicInfo/basic_info_start.dart';
+import 'package:hebtus_crossplatform/models/location.dart';
+import 'package:hebtus_crossplatform/screens/Creator/Tickets/add_promo_code.dart';
+import 'package:hebtus_crossplatform/screens/Creator/Tickets/edit_ticket.dart';
+void main() {
+  CurrentUser currentUser = CurrentUser();
+  currentUser.currentUser.firstName='xas';
+  currentUser.currentUser.lastName='asdx';
+  DateTime start=DateTime(2000);
+  DateTime end=DateTime(2100);
+  Location egypt=Location(longitude: 1, latitude: 2);
+  final CreatorEvent eventdetails=CreatorEvent(eventID: '1', eventName:' eventName', imgURL: 'imgURL', startTime: start, endTime: end, location: egypt, locationName: 'locationName', category: 'category', isDraft: true);
+
+  group("scrollTestingWidget", () {
+    testWidgets("should scroll ", (WidgetTester tester) async {
+      await tester.pumpWidget( MaterialApp(home: EditTicket(eventIDhere: "22", ticketIDHere: "22")));
+
+      await tester.drag(find.byType(Scaffold), const Offset(0, -200));
+      await tester.pump();
+    });
+  });
+}
